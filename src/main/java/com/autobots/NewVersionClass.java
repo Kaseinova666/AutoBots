719efdad6b2d7e38e74c136642cae53b82abe189
Aibyike <aibyikekaseinova@gmail.com>
2025-07-09 21:24:11 +0600

    Revert "Version 4.12.4" Bag in Demo class
    This reverts commit 506baf442f68e292ca89e99341d919882b93bf3a.

--- src/main/java/com/autobots/NewVersionClass.java
+++ src/main/java/com/autobots/NewVersionClass.java
@@ -1,6 +1,6 @@
 package com.autobots;
 
-public class Search {
+public class NewVersionClass {
     public static void main(String[] args) {
         System.out.println("New version 4.12.4");
     }
